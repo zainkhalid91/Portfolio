b13855b94d916a2af505f93d877e6095d9397580
Zain Ali Khalid <zain.ali@nitb.gov.pk>
2021-06-29 16:26:36 +0500

    minor fix

--- src/components/achievementCard/AchievementCard.js
+++ src/components/achievementCard/AchievementCard.js
@@ -8,7 +8,7 @@ export default function AchievementCard({cardInfo, isDark}) {
   }
 
   return (
-    <div className={isDark ? "dark-mode certificate-card" : "certificate-card"}>
+    <div className ={isDark ? "dark-mode certificate-card" : "certificate-card"}>
       <div className="certificate-image-div">
         <img src={cardInfo.image} alt="PWA" className="card-image"></img>
       </div>
