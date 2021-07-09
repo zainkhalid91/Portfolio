71eaf47519389e752a6d639e598d2ead192914cf
Zain Ali Khalid <zain.ali@nitb.gov.pk>
2021-07-09 15:36:37 +0500

    Updates

---
+++ precache-manifest.8ccee52601934c5c7f174cbd3b4671fd.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8af6f9a44a4390d8e628dd9610407a7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "22beae583413d979d58a",
+    "url": "/Portfolio/static/css/3.66597e91.chunk.css"
+  },
+  {
+    "revision": "96f7edadd89bed7309c3",
+    "url": "/Portfolio/static/css/4.0ef232e8.chunk.css"
+  },
+  {
+    "revision": "015163687f533f73c7df",
+    "url": "/Portfolio/static/css/main.81becdd3.chunk.css"
+  },
+  {
+    "revision": "8a94ef3e6280aa8cdc9e",
+    "url": "/Portfolio/static/js/2.371b6d09.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/Portfolio/static/js/2.371b6d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22beae583413d979d58a",
+    "url": "/Portfolio/static/js/3.63d402bc.chunk.js"
+  },
+  {
+    "revision": "96f7edadd89bed7309c3",
+    "url": "/Portfolio/static/js/4.f0faf771.chunk.js"
+  },
+  {
+    "revision": "015163687f533f73c7df",
+    "url": "/Portfolio/static/js/main.731fdb78.chunk.js"
+  },
+  {
+    "revision": "4bc180437194b10d714c",
+    "url": "/Portfolio/static/js/runtime-main.11e7e7ec.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/Portfolio/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/Portfolio/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "07698c066d2f41cab58b71523fb4fe8d",
+    "url": "/Portfolio/static/media/abbcAppLogo.07698c06.png"
+  },
+  {
+    "revision": "898b5ad15b6513a90030c0a4a405d919",
+    "url": "/Portfolio/static/media/cityAppLogo.898b5ad1.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "07bf44c8a74d89e136d40fda3454dd37",
+    "url": "/Portfolio/static/media/covid19Logo.07bf44c8.jpg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "93763eb43a462f73d604eb7811b07933",
+    "url": "/Portfolio/static/media/g50.93763eb4.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "9a4b2f8127754ec7ae93bd283371cfd0",
+    "url": "/Portfolio/static/media/ic_ezdealz_logo.9a4b2f81.png"
+  },
+  {
+    "revision": "822ac7e43d379abdc64f62f1431d4103",
+    "url": "/Portfolio/static/media/littleBirdLogo.822ac7e4.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "768fae23b399b31011da5ba07fb55a7c",
+    "url": "/Portfolio/static/media/nitbLogo.768fae23.png"
+  },
+  {
+    "revision": "7f178101a6c704fdce77cfc98e77cd37",
+    "url": "/Portfolio/static/media/numlLogo.7f178101.png"
+  },
+  {
+    "revision": "b31075ed7ed31dcf13504bfaa62aeb72",
+    "url": "/Portfolio/static/media/passtrackLogo.b31075ed.png"
+  },
+  {
+    "revision": "7c66d63e8d4d1b9d8630728eb07d212b",
+    "url": "/Portfolio/static/media/pmsLogo.7c66d63e.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "ede501ea078310c4371d2c963e810687",
+    "url": "/Portfolio/static/media/quellxcodeLogo.ede501ea.png"
+  },
+  {
+    "revision": "63e8f328cc65e8cd200d52f834c56bc4",
+    "url": "/Portfolio/static/media/reactiveStoresLogo.63e8f328.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "10a312b0c5ab3ffd0b1d73513a0ff5d2",
+    "url": "/Portfolio/static/media/tyslinLogo.10a312b0.png"
+  }
+]);
