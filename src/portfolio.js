d4e73eab07d50232c366ec813daf4a56aaf9c816
Zain Ali Khalid <zain.ali@nitb.gov.pk>
2021-07-09 15:34:42 +0500

    removed github profile

--- src/portfolio.js
+++ src/portfolio.js
@@ -191,8 +191,8 @@ const workExperiences = {
 To know how to get github key look at readme.md */
 
 const openSource = {
-  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  showGithubProfile: "false", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
